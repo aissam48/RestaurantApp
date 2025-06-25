2f8d8677b5f2f13cf717ca2be2bbba702c27106a
aissamelboudi <elboudi.04@email.com>
2025-06-25 09:19:44 +0100

    cleaning code

--- composeApp/src/commonMain/kotlin/com/findresto/project/navigation/NavigationHost.kt
+++ composeApp/src/commonMain/kotlin/com/findresto/project/navigation/NavigationHost.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.findresto.project.navigation.NavRoutes
 import com.findresto.project.screens.RestaurantsScreen
 
 @Composable
